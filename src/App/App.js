579610efab368ddd852d3ca03ece5a99e0f04f3b
Anson <1072476988@qq.com>
2020-10-27 22:01:57 +0800

    [Lingzhen Kong] fetch data when grouping

--- src/App/App.js
+++ src/App/App.js
@@ -6,6 +6,12 @@ class App extends Component {
     super(props);
     this.state = {
       students: [],
+      team1: [],
+      team2: [],
+      team3: [],
+      team4: [],
+      team5: [],
+      team6: [],
     };
   }
 
@@ -14,11 +20,23 @@ class App extends Component {
     const response = await fetch(URL);
     const data = await response.json();
     const map = new Map(Object.entries(data));
-    const arr = [...map]; // 转数组方法1
+    const arr = [...map];
     console.log(arr);
     this.setState({
       students: arr,
     });
+  };
+
+  handleClick = async () => {
+    const URL = 'http://localhost:8080/grouping';
+    const response = await fetch(URL);
+    const data = await response.json();
+    data.map((students) => {
+      const map = new Map(Object.entries(students));
+      const arr = [...map];
+      console.log(arr);
+    });
+    console.log(data);
   };
 
   render() {
@@ -27,7 +45,7 @@ class App extends Component {
         <div className="GroupingList">
           <div className="GroupingTab">
             <label>分组列表</label>
-            <input type="button" value="分组学员" />
+            <input type="button" value="分组学员" onClick={this.handleClick} />
           </div>
         </div>
         <div className="StudentList">
